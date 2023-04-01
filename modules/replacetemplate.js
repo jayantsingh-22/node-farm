d917f50ce930441b46d60a8af27f8522ffc82227
jayantsingh-22 <jayantsingh640@gmail.com>
2023-04-02 02:29:50 +0530

    exports defined

--- modules/replacetemplate.js
+++ modules/replacetemplate.js
@@ -1,4 +1,4 @@
-module.exports  = (temp,product) => {
+const hello = (temp,product) => {
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
@@ -12,3 +12,5 @@ module.exports  = (temp,product) => {
   
     return output;
   } 
+
+  export default hello;
